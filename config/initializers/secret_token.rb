12c411a131194f4bf21ad015ec535834715b8303
Faith-Anne Kocadag <faith.anne@gmail.com>
2013-12-17 16:06:44 -0500

    Initial commit, create new rails project.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mosh2::Application.config.secret_token = 'b655052ea6f198f2bfb3c0c2ef89f04acc9975dbe1630fcc275f2229d9915bf46d2a10c78f0df48685a96233bf071ff210cfbd2b972b6863d227e0e3126b7b2a'
+Mosh2::Application.config.secret_token = '5cfbb8034622778570aea688c79be6c522df74e415471d9417a6e815b309c9f8fb99cf4261bce14d491f74a19726933347cdda614b5fabcfba3fb495884a0be7'
