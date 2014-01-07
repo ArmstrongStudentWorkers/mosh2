40be3fa4c4d395ad605cf0072c07dca007f923d2
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-01-07 15:33:05 -0500

    Add tabs to Inventory

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -4,5 +4,15 @@ class InventoryController < ApplicationController
     if params[:location]
       @location = Location.find(params[:location])
     end
+
+    @hardware_statuses = HardwareStatus.all
+    if params[:hardware_status]
+      @hardware_status = HardwareStatus.find(params[:hardware_status])
+    end
+
+    @hardware_types = HardwareType.all
+    if params[:hardware_type]
+      @hardware_type = HardwareType.find(params[:hardware_type])
+    end
   end
 end
