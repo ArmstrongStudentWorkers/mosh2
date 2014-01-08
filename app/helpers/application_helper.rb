2ea2e989e99b61ce5858634d8c92dfb7c6a137a1
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-01-08 01:16:39 -0500

    Create and format views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "MOSH: The McCarthy Online Support Hub"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
