1dd606078194b5a3a03b3f8b82f0b6bef32c4847
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-01-08 21:36:59 -0500

    Making inventory search work.
    * Used AJAX to populate input select.
    * Had inventory page render different partials according to
    selected search type.
    * Had inventory controller search for different results
    according to search type.

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -1,5 +1,33 @@
 $(function () {
   $(".btn-group button").click(function () {
-    $("#search-type").val($(this).text());
+    var $select     = $('#select').empty();
+    var button_text = $(this).text();
+    var url         = '';
+
+    $("#search_type").val(button_text);
+
+    if (button_text === 'Location') {
+      url = 'locations.json';
+    }
+    else if (button_text === 'Status') {
+      url = 'hardware_statuses.json';
+    }
+    else if (button_text === 'Type') {
+      url = 'hardware_types.json';
+    }
+    else {
+      console.log('Unknown button pressed.');
+      url = '';
+    }
+    $.ajax({type: 'GET',
+      url: url,
+      success: function(msg) {
+        $.each(msg, function(index, value) {
+          console.log(value.id + ',' + value.name);
+          $select.append('<option value="' +
+            value.id + '">' + value.name + '</option>');
+        });
+      }
+    });
   });
 });
--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -5,19 +5,15 @@ include MoshModules::ManagementModule
 before_filter :management_filter
 
   def index
-    @locations = Location.all
-    if params[:location]
-      @location = Location.find(params[:location])
-    end
-
-    @hardware_statuses = HardwareStatus.all
-    if params[:hardware_status]
-      @hardware_status = HardwareStatus.find(params[:hardware_status])
-    end
-
-    @hardware_types = HardwareType.all
-    if params[:hardware_type]
-      @hardware_type = HardwareType.find(params[:hardware_type])
+    if params[:search_type]
+      search_type = params[:search_type]
+      if search_type == 'Location'
+        @location = Location.find(params[:select])
+      elsif search_type == 'Status'
+        @hardware_status = HardwareStatus.find(params[:select])
+      elsif search_type == 'Type'
+        @hardware_type = HardwareType.find(params[:select])
+      end
     end
   end
 end
