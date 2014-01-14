1b14723bcf8e1ce9ce3f163a7b119211dd0ca174
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-01-14 16:00:58 -0500

    Remove hour from semester

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,6 +1,5 @@
 class Semester < ActiveRecord::Base
   attr_accessible :advisement_end, :advisement_start, :end_date, :name, :start_date, :term_id, :year
   belongs_to :term
-  has_many :hours
   has_many :after_hours_accesses
 end
