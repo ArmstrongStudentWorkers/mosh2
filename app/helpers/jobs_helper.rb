f738014924fefb94e4e6c9fb45f4e1e9134b5ded
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-02-03 14:26:11 -0500

    Adding queue number to Job.show view.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,18 @@
 module JobsHelper
+
+  def get_queue(job)
+    if job.job_status.name != "Pending" 
+      "n/a"
+    else
+      count = 0
+      pending = (Job.where(job_status_id: JobStatus.where(name: "Pending").first.id) && Job.where("id < ?", job.id))
+      
+      pending.each do |j|
+        count += j.posters.count
+      end
+      count
+
+    end
+  end
+
 end
