998b6f558fc5f291785f8f351950a25de486c7c0
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-02-12 18:47:54 -0500

    Adding status helper to application helper and removing it from jobs helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,19 @@ module ApplicationHelper
     end
   end
 
+  def get_status(status)
+    if status == "Pending"
+      "<span class='label label-warning'>#{status}</span>".html_safe
+    elsif status == "Finished"
+      "<span class='label label-success'>#{status}</span>".html_safe
+    elsif status == "Denied"
+      "<span class='label label-important'>#{status}</span>".html_safe
+    else
+      ""
+    end
+  end
+
+
   def active_page?(page, current_step)
     'active' if page == current_step
   end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,24 +1,12 @@
 module JobsHelper
 
-  def get_job_status(status)
-    if status == "Pending"
-      "<span class='label label-warning'>#{status}</span>".html_safe
-    else if status == "Finished"
-      "<span class='label label-success'>#{status}</span>".html_safe
-    else if status == "Denied"
-      "<span class='label label-important'>#{status}</span>".html_safe
-    else
-
-    end
-  end
-
   def get_queue(job)
     if job.job_status.name != "Pending" 
       "n/a"
     else
       count = 0
       pending = (Job.where(job_status_id: JobStatus.where(name: "Pending").first.id) && Job.where("id < ?", job.id))
-      
+
       pending.each do |j|
         count += j.posters.count
       end
