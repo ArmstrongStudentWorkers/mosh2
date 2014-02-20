430f27a7ce2738eabba6de81a6a177f5dd6c0479
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-02-19 23:13:34 -0500

    Test

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, 'MOSH'
 set :repo_url, 'git@github.com:ArmstrongStudentWorkers/mosh2.git'
 set :branch, 'master'
 
+# Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
