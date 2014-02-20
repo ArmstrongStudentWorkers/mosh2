8b690831b74c88b3185b4003fc5b2d580197b326
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-02-19 22:44:22 -0500

    Fixing spacing in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'MOSH'
+set :repo_url, 'git@github.com:ArmstrongStudentWorkers/mosh2.git'
+set :branch, 'master'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/mosh'
 
 # Default value for :scm is :git
 # set :scm, :git
