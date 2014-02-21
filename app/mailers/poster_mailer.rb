3719df70bf748f6b00dfed96e598e58a81a75eb5
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-02-21 01:03:14 -0500

    Formatting email urls.

--- app/mailers/poster_mailer.rb
+++ app/mailers/poster_mailer.rb
@@ -5,7 +5,7 @@ class PosterMailer < ActionMailer::Base
     @user = User.where(id: current_user_id).first
     @poster_overview = PosterOverview.where(id: poster_overview_id).first
     @date = date
-    @url = 'http://mosh.c-mccarthy.com/poster_overview/#{poster_overview_id}'
+    @url = "http://mosh.c-mccarthy.com:3000/poster_overview/#{poster_overview_id}"
     mail(to: @user.email, subject: "Your Poster Job #{poster_overview_id}")
   end
 end
