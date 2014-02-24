3d8d2857d7fafcf9c921ae8bb44e399839a1697e
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-02-23 20:45:19 -0500

    Configuring smtp in production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Mosh2::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'mosh.c-mccarthy.com' }
+  config.action_mailer.delivery_method = :smtp
 end
