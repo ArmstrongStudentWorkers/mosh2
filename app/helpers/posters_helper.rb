d678cf9326c5853bacb463a5003c373de2e7040e
Faith-Anne Kocadag <faith.anne@gmail.com>
2014-03-01 09:57:12 -0500

    Replacing google iframe with embedded object in poster.show.

--- app/helpers/posters_helper.rb
+++ app/helpers/posters_helper.rb
@@ -3,4 +3,9 @@ module PostersHelper
     url.slice!(0)
     "https://docs.google.com/gview?url=#{root_url}#{url}&embedded=true"
   end
+
+  def get_url(url)
+    url.slice!(0)
+    "#{root_url}#{url}"
+  end
 end
